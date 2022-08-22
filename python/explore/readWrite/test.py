1f78144cf68382c5cc1da5c610382a27e5cd3492
rezimaulana <rsazrm@gmail.com>
2022-08-23 00:06:58 +0700

    py read write file

---
+++ python/explore/readWrite/test.py
@@ -0,0 +1,6 @@
+""" with open("E:\Development\playScript\kbpa\session2\output8.txt", "w") as f:
+	f.write('Test')
+ """
+with open("E:\Development\playScript\kbpa\session2\output8.txt", "w") as f:
+    for i in range(10):
+        f.write('Test\n')
