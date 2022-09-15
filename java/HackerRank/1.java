b75d53bb3630893e043ce64ef4b77e23e05aa8a8
rezimaulana <rsazrm@gmail.com>
2022-09-15 19:56:03 +0700

    java setup project

---
+++ java/HackerRank/1.java
@@ -0,0 +1,14 @@
+//Welcome to Java!
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        Solution app = new Solution();
+        app.output();
+    }
+    
+    public void output(){
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
---
+++ java/HackerRank/5.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+                
+                System.out.print(s1);
+                for(int j = s1.length(); j< 15; j++){
+                    System.out.print(" ");
+                }
+                if(x>=100){
+                    System.out.print(x);
+                }else if (x<100 && x >=10){
+                    System.out.print("0"+x);
+                }else if(x<10){
+                    System.out.print("00"+x);
+                }
+                System.out.println();
+                
+            }
+            System.out.println("================================");
+
+    }
+}
+
+
+
